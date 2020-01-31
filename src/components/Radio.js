2d39e6637cfb509ab51b188220d4dbeccb9585a9
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-30 20:59:01 -0800

    check button for selected method by default.

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -2,13 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Radio.css';
 
-const Radio = ({ method, onChange }) => (
+const Radio = ({ selectedMethod, method, onChange }) => (
   <>
-    <input id={method} onChange={onChange} type="radio" name="method" value={method} />
+    <input id={method} 
+      onChange={onChange} 
+      type="radio" 
+      name="method" 
+      value={method} 
+      checked={selectedMethod === method} />
     <label htmlFor={method} className={styles.Radio}>{method}</label>
   </>);
 Radio.propTypes = {
   method: PropTypes.string.isRequired,
+  selectedMethod: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired
 };
 
